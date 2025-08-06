8a49a44d72bfc8148694ffe0b0277b694cf11186
yvesd <yves.daetwyler@gmail.com>
2025-08-06 15:41:48 +0200

    destructure, make sortable list for problems, create yet empty problem 04

---
+++ src/problems/Problem_04.js
@@ -0,0 +1,13 @@
+import { useState, useEffect } from "react";
+import Solution from "../components/Solution"
+
+const Problem_04 = ({ digits }) => {
+
+    return (
+        <>
+            <Solution solutionValue={''} />
+        </>
+    )
+}
+
+export default Problem_04;
