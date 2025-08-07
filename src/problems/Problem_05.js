518204e0fdb3bb41f0ccfdc06a647028b0deae1a
yvesd <yves.daetwyler@gmail.com>
2025-08-07 09:37:12 +0200

    solve problem 5 (elegant solution :D)

--- src/problems/Problem_05.js
+++ src/problems/Problem_05.js
@@ -1,11 +1,39 @@
 import { useState, useEffect } from "react";
 import Solution from "../components/Solution";
 
-const Problem_05 = () => {
+const Problem_05 = ({ maxNum }) => {
+    const [solutionValue, setSolutionValue] = useState(0);
+
+    // Greatest common divisor
+    const getGCD = (a, b) => {
+        while (b !== 0) {
+            let temp = b;
+            b = a % b;
+            a = temp;
+        } 
+        return a;
+    }
+
+    // Least common multiple
+    const getLCM = (a, b) => (a * b) / getGCD(a, b);
+
+    useEffect(() => {
+        // Check if prop is Int and positive
+        if (maxNum <= 0 || !Number.isInteger(maxNum)) return;
+
+        // Find the least common multiple of all numbers from 1 to maxNum
+        let currentLCM = 1;
+        for (let i = 1; i <= maxNum; i++) {
+            const lcm = getLCM(currentLCM, i);
+            currentLCM = lcm;
+        }
+        
+        setSolutionValue(currentLCM);
+    }, [maxNum])
 
     return (
         <>
-            <Solution solutionValue={''} />
+            <Solution solutionValue={solutionValue} />
         </>
     )
 }
