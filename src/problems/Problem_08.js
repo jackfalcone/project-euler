bab62abe16d0e47463fdb6ec0d6a153b16bf2247
yvesd <yves.daetwyler@gmail.com>
2025-08-09 11:51:09 +0200

    add problem 8

--- src/problems/Problem_08.js
+++ src/problems/Problem_08.js
@@ -1,7 +1,15 @@
 import { useState, useEffect } from "react";
 import Solution from "../components/Solution";
 
-const Problem_08 = () => {
+const Problem_08 = ({ digits, numStr }) => {
+    const [number, setNumber] = useState(0);
+
+    /* global BigInt */
+    useEffect(() => {
+        if (numStr) {
+            setNumber(BigInt(numStr));
+        }
+    }, [numStr])
 
     return (
         <>
